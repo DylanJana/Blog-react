b3ae55363472f22b05a28930658f0d5c5090ec09
Dylan Durand <dylan.durand88@outlook.fr>
2023-04-10 17:09:59 +0200

    Liaison du formulaire avec le state
    Mise en place de la logique, state, handleForm, handleInputs

--- blog/src/Components/Form/Form.js
+++ blog/src/Components/Form/Form.js
@@ -1,22 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+// Pour envoyer des infos
+import {useDispatch} from 'react-redux';
 import './Form.css'
+import { handleError } from 'vue';
 
 export default function Form() {
+  // Création du state + méthode de modification
+  const [article, setArticle] = useState({
+    title: '',
+    body: ''
+  })
+
+  const dispatch = useDispatch();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  }
+
+  const handleInputs = (e) => {
+    if(e.target.classList.contains('inp-title')) {
+      // Je ne change pas le state directement je crée donc un nouvel objet.
+      const newObjState = {
+        // Je copie le state article
+        ...article,
+        // J'attribue à la propriété title du state article, la valeur courante de mon input
+        title: e.target.value
+      }
+      // Je passe en argument de ma méthode qui me permet de changer le state, newObjState
+      // Le state sera modifié
+      setArticle(newObjState)
+    } else if(e.target.classList.contains('inp-body')) {
+      const newObjState = {
+        // Je copie le state article
+        ...article,
+        // J'attribue à la propriété title du state article, la valeur courante de mon input
+        body: e.target.value
+      }
+      // Je passe en argument de ma méthode qui me permet de changer le state, newObjState
+      // Le state sera modifié
+      setArticle(newObjState)
+    }
+  }
   return (
     <>
       <h1 className='title-form'>Écrivez un article</h1>
-      <form className="container-form">
+      <form onSubmit={handleForm} className="container-form">
         <label htmlFor="title">Titre</label>
-        <input 
+        <input
+          // onInput trigger l'évènement à chaque fois que j'écris quelque chose dans l'input
+          //onChange trigger uniquement quand je perds le focus
+          onChange={handleInputs}
+          // La valeur entré dans l'input donnera la valeur de la propriété title de mon state
+          value={article.title}
           type="text" 
           id='title' 
-          placeholder='Entrez votre nom' 
+          placeholder='Entrez votre nom'
+          className='inp-title' 
         />
         <label htmlFor="article">Votre article</label>
         <textarea 
+          onChange={handleInputs}
+          // La valeur entré dans le textarea donnera la valeur de la propriété body de mon state
+          value={article.body}
           id="article"
           placeholder='Votre article'
-        ></textarea>
+          className='inp-body'
+        >
+        </textarea>
         <button type='submit'>Envoyer l'article</button>
       </form>
     </>
